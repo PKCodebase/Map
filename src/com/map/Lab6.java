89d3c67782748c693ee78cf05adb701ae983cf30
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-05 09:11:04 +0530

    Lab6

--- src/com/map/Lab6.java
+++ src/com/map/Lab6.java
@@ -7,7 +7,6 @@ import java.util.Set;
 public class Lab6 {
     public static void main(String []args){
         Map<String,String> mymap = new HashMap<>();
-
         mymap.put("sid", "101");
         mymap.put("name", "kaushik");
         mymap.put("phone","12345");
